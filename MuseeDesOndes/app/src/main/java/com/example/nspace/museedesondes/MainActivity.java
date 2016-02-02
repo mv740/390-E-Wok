3831f0a908e8a794f80afff62be4ba8cf79c4d41
Sebastian Shah <sebshah@hotmail.com>
2016-02-02 18:12:18 -0500

    Added settings activity for language selection EWOK-41

--- MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/MainActivity.java
+++ MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.nspace.museedesondes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button settings_button = (Button) findViewById(R.id.settings_button);
+        settings_button.setOnClickListener(
+                 new Button.OnClickListener() {
+                    public void onClick(View view) {
+                        Intent startSettings = new Intent(MainActivity.this, Settings.class);
+                        startActivity(startSettings);
+                    }
+                 }
+        );
     }
 }
---
+++ MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/Settings.java
@@ -0,0 +1,17 @@
+package com.example.nspace.museedesondes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Settings extends AppCompatActivity {
+
+    /**
+     * Created by sebastian on 2/02/2016.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}

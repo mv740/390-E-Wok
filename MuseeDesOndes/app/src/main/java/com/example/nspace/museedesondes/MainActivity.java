5d93570108ede4e2db73e0bc185552f0af115a0d
Sebastian Shah <sebshah@hotmail.com>
2016-02-02 23:36:05 -0500

    App can now change between French and English language in settings. When hitting back to launch screen if language changed activity recreated. EWOK-42

--- MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/MainActivity.java
+++ MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/MainActivity.java
@@ -6,21 +6,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
-    
+
+    /**
+     * Created by sebastian on 2/02/2016.
+     */
+
+    String currentLanguage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Locale currentLocale = getResources().getConfiguration().locale;
+        currentLanguage = currentLocale.getLanguage();
+
         final Button settings_button = (Button) findViewById(R.id.settings_button);
         settings_button.setOnClickListener(
-                 new Button.OnClickListener() {
+                new Button.OnClickListener() {
                     public void onClick(View view) {
                         Intent startSettings = new Intent(MainActivity.this, Settings.class);
                         startActivity(startSettings);
                     }
-                 }
+                }
         );
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Locale current = getResources().getConfiguration().locale;
+        if(!currentLanguage.equals(current.getLanguage())) {
+            recreate();
+        };
     }
 }
--- MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/Settings.java
+++ MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/Settings.java
@@ -1,7 +1,16 @@
 package com.example.nspace.museedesondes;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Locale;
 
 public class Settings extends AppCompatActivity {
 
@@ -9,9 +18,48 @@ public class Settings extends AppCompatActivity {
      * Created by sebastian on 2/02/2016.
      */
 
+    Locale myLocale;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+
+
+        //set english language
+        final Button english_button = (Button) findViewById(R.id.english_button);
+        english_button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view) {
+                        setLocale("en_US");
+                    }
+                }
+        );
+
+
+        //set french language
+        final Button french_button = (Button) findViewById(R.id.french_button);
+        french_button.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View view) {
+                        setLocale("fr");
+                    }
+                }
+        );
+
+
     }
+    public void setLocale(String lang) {
+        myLocale = new Locale(lang);
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(this, Settings.class);
+        startActivity(refresh);
+        finish();
+    }
+
 }

2f59dc98dfdeadaaedb7b88175e25fa4af6aa3c7
francis cote-tremblay <franciscotetremblay@gmail.com>
2016-02-09 19:51:15 -0500

    -added proper homescreen -refactor of handlers in the mainactivity

--- MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/MainActivity.java
+++ MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/MainActivity.java
@@ -5,9 +5,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 
 import com.example.nspace.museedesondes.Model.Preferences;
 
@@ -31,27 +34,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Button settings_button = (Button) findViewById(R.id.settings_button);
-        settings_button.setOnClickListener(
-                new Button.OnClickListener() {
-                    public void onClick(View view) {
-                        Intent startSettings = new Intent(MainActivity.this, Settings.class);
-                        startActivity(startSettings);
-                    }
-                }
-        );
-
-
-        final Button exploration_button = (Button) findViewById(R.id.begin_tour_button);
-        exploration_button.setOnClickListener(
-                new Button.OnClickListener(){
-                    public void onClick(View v){
-                        Intent  startStorylines = new Intent(MainActivity.this, StoryLineActivity.class);
-                        startActivity(startStorylines);
-                    }
-                }
-        );
+        loadReplaceMeWith(R.layout.welcome_language);
     }
+
+    private void loadReplaceMeWith(int id){
+        FrameLayout replaceMe = (FrameLayout) findViewById(R.id.welcome_replace_me);
+        replaceMe.removeAllViews();
+        LayoutInflater li = LayoutInflater.from(this.getBaseContext());
+        RelativeLayout replacer = (RelativeLayout) li.inflate(id, null);
+        replaceMe.addView(replacer);
+    }
+
+
+    //HANDLERS ****************************************
+
+    public void changeLanguageOnClickFr(View view){
+        changeLanguage("fr");
+        loadReplaceMeWith(R.layout.welcome_begin_tour);
+    }
+    public void changeLanguageOnClickEn(View view){
+        changeLanguage("en_US");
+        loadReplaceMeWith(R.layout.welcome_begin_tour);
+    }
+
+    private void changeLanguage(String lang){
+        Preferences.setLocale(lang);
+        Preferences.savePreferences(lang);
+    }
+
+    public void beginTourOnClick(View view) {
+        Intent  startStorylines = new Intent(MainActivity.this, StoryLineActivity.class);
+        startActivity(startStorylines);
+    }
+
+    public void settingsOnClick(View view){
+        Intent startSettings = new Intent(MainActivity.this, Settings.class);
+        startActivity(startSettings);
+    }
+
 
 
 

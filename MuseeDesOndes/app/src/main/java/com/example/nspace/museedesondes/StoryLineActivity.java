494c50eb09942edc129e6338fab2a9bf81f61495
Sebastian Shah <sebshah@hotmail.com>
2016-02-12 20:27:39 -0500

    Refactor storyline activity. Pass storyline title to map activity EWOK-12

--- MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/StoryLineActivity.java
+++ MuseeDesOndes/app/src/main/java/com/example/nspace/museedesondes/StoryLineActivity.java
@@ -13,34 +13,35 @@ import java.util.Locale;
 
 public class StoryLineActivity extends AppCompatActivity {
 
-
+    ListView list;
+    String[] titles;
+    String[] description;
+    Integer[] imageId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_story_line);
 
-
         Locale currentLocale = getResources().getConfiguration().locale;
         String currentLanguage = currentLocale.getLanguage();
 
         //todo: fetch storyline title/description/image array from Map and put into arrays based on currentLanguage
-        ListView list;
-        String[] titles = {
+        titles = new String[]{
                 "Free Exploration",
                 "RCA Throughout history",
                 "Nippers the Dog",
                 "Gramaphone Stuff",
         } ;
 
-        String[] description = {
+        description = new String[]{
                 "Explore all of the exhbits in the museum in any order.",
                 "Before the publication of the First Folio in 1623, nineteen of the thirty-seven plays in Shakespeare's canon had appeared in quarto format. ",
                 "With the exception of Othello (1622), all of the quartos were published prior to the date of Shakespeare's retirement from the theatre in about 1611",
                 "Here you will find the complete text of Shakespeare's plays, based primarily on the First Folio, and a variety of helpful resources, including extensive explanatory notes, character analysis."
         } ;
 
-        Integer[] imageId = {
+        imageId = new Integer[]{
                 R.drawable.free_exploration,
                 R.drawable.placeholder_home_icon,
                 R.drawable.placeholder_panda_icon,
@@ -55,14 +56,9 @@ public class StoryLineActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view,
                                     int position, long id) {
-                if (position == 0) {
-                    Intent startMap = new Intent(StoryLineActivity.this, MapActivity.class);
-                    startActivity(startMap);
-                } else {
-                    //todo: addExtra() passing storyline ID
-                    Intent startMap = new Intent(StoryLineActivity.this, MapActivity.class);
-                    startActivity(startMap);
-                }
+                Intent startMap = new Intent(StoryLineActivity.this, MapActivity.class);
+                startMap.putExtra("title",titles[position]);
+                startActivity(startMap);
             }
         });
     }
